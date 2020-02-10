979054000f82260086948df424c1d352a08db7dc
yasasvy gunturu <guntur.yasasvy@gmail.com>
2020-02-10 21:40:46 +0530

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    render() {
+        return (
+          <form onSubmit={this.props.getWeather}>
+            <input type="text" name="city" placeholder="City..."/>
+            <input type="text" name="country" placeholder="Country..."/>
+            <button>Get Weather</button>
+          </form>
+        )
+    }
+}
